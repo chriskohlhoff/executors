444be82e73cee63e75626c62c65664ba12596c32
Christopher Kohlhoff <chris@kohlhoff.com>
2014-03-08 00:11:17 +1100

    Skeleton of execution_context's id and service classes.

--- include/experimental/bits/execution_context.h
+++ include/experimental/bits/execution_context.h
@@ -12,15 +12,65 @@
 #ifndef EXECUTORS_EXPERIMENTAL_BITS_EXECUTION_CONTEXT_H
 #define EXECUTORS_EXPERIMENTAL_BITS_EXECUTION_CONTEXT_H
 
+#include <stdexcept>
+
 namespace std {
 namespace experimental {
 
-execution_context::execution_context()
+inline execution_context::execution_context()
 {
 }
 
-execution_context::~execution_context()
+inline execution_context::~execution_context()
 {
+}
+
+inline void execution_context::shutdown()
+{
+}
+
+inline execution_context::id::id()
+{
+}
+
+inline execution_context::service::service(execution_context& __c)
+  : _M_context(__c), _M_next(0)
+{
+}
+
+inline execution_context::service::~service()
+{
+}
+
+inline execution_context& execution_context::service::context()
+{
+  return _M_context;
+}
+
+class service_already_exists
+  : logic_error
+{
+public:
+  service_already_exists()
+    : logic_error("Service already_exists.")
+  {
+  }
+};
+
+template <class _Service> _Service& use_service(execution_context& __c)
+{
+  throw std::bad_cast();
+}
+
+template <class _Service, class... _Args> _Service&
+  make_service(execution_context& __c, _Args&&... __args)
+{
+  throw std::bad_cast();
+}
+
+template <class _Service> bool has_service(execution_context& __c)
+{
+  return false;
 }
 
 } // namespace experimental
