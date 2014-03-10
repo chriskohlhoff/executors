d62d77ef86f5384e09405a626dc7488455965c20
Christopher Kohlhoff <chris@kohlhoff.com>
2014-03-10 21:04:09 +1100

    More tests for dispatch_after and dispatch_at.

---
+++ src/tests/executor/dispatch_after_int.cpp
@@ -0,0 +1,139 @@
+#include <experimental/executor>
+#include <experimental/future>
+#include <experimental/loop_scheduler>
+#include <experimental/timer>
+#include <cassert>
+#include <stdexcept>
+
+int function_count = 0;
+int handler_count = 0;
+
+int function1()
+{
+  return ++function_count;
+}
+
+struct function2
+{
+  function2() {}
+  int operator()() { return ++function_count; }
+};
+
+struct function3
+{
+  function3() {}
+  function3(const function3&) = delete;
+  function3(function3&&) {}
+  int operator()() { return ++function_count; }
+};
+
+void handler1(int)
+{
+  ++handler_count;
+}
+
+struct handler2
+{
+  handler2() {}
+  void operator()(int) { ++handler_count; }
+};
+
+struct handler3
+{
+  handler3() {}
+  handler3(const handler3&) = delete;
+  handler3(handler3&&) {}
+  void operator()(int) { ++handler_count; }
+};
+
+int main()
+{
+  function2 f2;
+  const function2 cf2;
+  function3 f3;
+
+  handler2 h2;
+  const handler2 ch2;
+  handler3 h3;
+
+  std::experimental::loop_scheduler scheduler;
+  std::experimental::executor ex = get_executor(scheduler);
+
+  ex = std::experimental::system_executor();
+  assert(&ex.context() == &std::experimental::system_executor().context());
+
+  auto rel_time = std::chrono::milliseconds(100);
+
+  std::experimental::dispatch_after(rel_time, function1, ex, handler1);
+  std::experimental::dispatch_after(rel_time, function1, ex, &handler1);
+  std::experimental::dispatch_after(rel_time, function1, ex, handler2());
+  std::experimental::dispatch_after(rel_time, function1, ex, h2);
+  std::experimental::dispatch_after(rel_time, function1, ex, ch2);
+  std::experimental::dispatch_after(rel_time, function1, ex, handler3());
+  std::experimental::dispatch_after(rel_time, function1, ex, std::move(h3));
+  std::future<int> fut1 = std::experimental::dispatch_after(rel_time, function1, ex, std::experimental::use_future);
+  fut1.get();
+
+  std::experimental::dispatch_after(rel_time, &function1, ex, handler1);
+  std::experimental::dispatch_after(rel_time, &function1, ex, &handler1);
+  std::experimental::dispatch_after(rel_time, &function1, ex, handler2());
+  std::experimental::dispatch_after(rel_time, &function1, ex, h2);
+  std::experimental::dispatch_after(rel_time, &function1, ex, ch2);
+  std::experimental::dispatch_after(rel_time, &function1, ex, handler3());
+  std::experimental::dispatch_after(rel_time, &function1, ex, std::move(h3));
+  std::future<int> fut2 = std::experimental::dispatch_after(rel_time, &function1, ex, std::experimental::use_future);
+  fut2.get();
+
+  std::experimental::dispatch_after(rel_time, function2(), ex, handler1);
+  std::experimental::dispatch_after(rel_time, function2(), ex, &handler1);
+  std::experimental::dispatch_after(rel_time, function2(), ex, handler2());
+  std::experimental::dispatch_after(rel_time, function2(), ex, h2);
+  std::experimental::dispatch_after(rel_time, function2(), ex, ch2);
+  std::experimental::dispatch_after(rel_time, function2(), ex, handler3());
+  std::experimental::dispatch_after(rel_time, function2(), ex, std::move(h3));
+  std::future<int> fut3 = std::experimental::dispatch_after(rel_time, function2(), ex, std::experimental::use_future);
+  fut3.get();
+
+  std::experimental::dispatch_after(rel_time, f2, ex, handler1);
+  std::experimental::dispatch_after(rel_time, f2, ex, &handler1);
+  std::experimental::dispatch_after(rel_time, f2, ex, handler2());
+  std::experimental::dispatch_after(rel_time, f2, ex, h2);
+  std::experimental::dispatch_after(rel_time, f2, ex, ch2);
+  std::experimental::dispatch_after(rel_time, f2, ex, handler3());
+  std::experimental::dispatch_after(rel_time, f2, ex, std::move(h3));
+  std::future<int> fut4 = std::experimental::dispatch_after(rel_time, f2, ex, std::experimental::use_future);
+  fut4.get();
+
+  std::experimental::dispatch_after(rel_time, cf2, ex, handler1);
+  std::experimental::dispatch_after(rel_time, cf2, ex, &handler1);
+  std::experimental::dispatch_after(rel_time, cf2, ex, handler2());
+  std::experimental::dispatch_after(rel_time, cf2, ex, h2);
+  std::experimental::dispatch_after(rel_time, cf2, ex, ch2);
+  std::experimental::dispatch_after(rel_time, cf2, ex, handler3());
+  std::experimental::dispatch_after(rel_time, cf2, ex, std::move(h3));
+  std::future<int> fut5 = std::experimental::dispatch_after(rel_time, cf2, ex, std::experimental::use_future);
+  fut5.get();
+
+  std::experimental::dispatch_after(rel_time, function3(), ex, handler1);
+  std::experimental::dispatch_after(rel_time, function3(), ex, &handler1);
+  std::experimental::dispatch_after(rel_time, function3(), ex, handler2());
+  std::experimental::dispatch_after(rel_time, function3(), ex, h2);
+  std::experimental::dispatch_after(rel_time, function3(), ex, ch2);
+  std::experimental::dispatch_after(rel_time, function3(), ex, handler3());
+  std::experimental::dispatch_after(rel_time, function3(), ex, std::move(h3));
+  std::future<int> fut6 = std::experimental::dispatch_after(rel_time, function3(), ex, std::experimental::use_future);
+  fut6.get();
+
+  std::experimental::dispatch_after(rel_time, std::move(f3), ex, handler1);
+  std::experimental::dispatch_after(rel_time, std::move(f3), ex, &handler1);
+  std::experimental::dispatch_after(rel_time, std::move(f3), ex, handler2());
+  std::experimental::dispatch_after(rel_time, std::move(f3), ex, h2);
+  std::experimental::dispatch_after(rel_time, std::move(f3), ex, ch2);
+  std::experimental::dispatch_after(rel_time, std::move(f3), ex, handler3());
+  std::experimental::dispatch_after(rel_time, std::move(f3), ex, std::move(h3));
+  std::future<int> fut7 = std::experimental::dispatch_after(rel_time, std::move(f3), ex, std::experimental::use_future);
+  fut7.get();
+
+  assert(function_count == 56);
+  assert(handler_count == 49);
+}
