chriskohlhoff/executors

commit db2bedc862d47da203c98c033f3003e02bf05d5b

Add recycling of small allocated blocks.

include/experimental/bits/scheduler.h
@@ -23,6 +23,7 @@
 
 #include <experimental/bits/call_stack.h>
 #include <experimental/bits/operation.h>
+#include <experimental/bits/small_block_recycler.h>
 
 namespace std {
 namespace experimental {
@@ -194,13 +195,15 @@ public:
 
   virtual void _Complete()
   {
-    unique_ptr<__scheduler_op> __op(this);
-    _M_func();
+    __small_block_recycler<>::_Unique_ptr<__scheduler_op> __op(this);
+    _Func __f(std::move(_M_func));
+    __op.reset();
+    __f();
   }
 
   virtual void _Destroy()
   {
-    delete this;
+    __small_block_recycler<>::_Destroy(this);
   }
 
 private:
@@ -211,8 +214,8 @@ private:
 template <class _F> void __scheduler::_Post(_F&& __f)
 {
   typedef typename decay<_F>::type _Func;
-  unique_ptr<__scheduler_op<_Func>> __op(
-    new __scheduler_op<_Func>(forward<_F>(__f), *this));
+  __small_block_recycler<>::_Unique_ptr<__scheduler_op<_Func>> __op(
+    __small_block_recycler<>::_Create<__scheduler_op<_Func>>(forward<_F>(__f), *this));
 
   lock_guard<mutex> lock(_M_mutex);
 

include/experimental/bits/wait_op.h
@@ -41,14 +41,16 @@ public:
 
   virtual void _Complete()
   {
-    unique_ptr<__wait_op> __op(this);
-    get_executor(_M_work).post(
-      __invoke_with_result<const error_code, _Func>{_M_ec, std::move(_M_func)});
+    __small_block_recycler<>::_Unique_ptr<__wait_op> __op(this);
+    auto __executor(get_executor(_M_work));
+    __invoke_with_result<const error_code, _Func> __i{_M_ec, std::move(_M_func)};
+    __op.reset();
+    __executor.post(std::move(__i));
   }
 
   virtual void _Destroy()
   {
-    delete this;
+    __small_block_recycler<>::_Destroy(this);
   }
 
 private:

include/Makefile.am
@@ -23,6 +23,7 @@ nobase_include_HEADERS = \
 	experimental/bits/promise_handler.h \
 	experimental/bits/reactor.h \
 	experimental/bits/scheduler.h \
+	experimental/bits/small_block_recycler.h \
 	experimental/bits/system_executor.h \
 	experimental/bits/thread_pool.h \
 	experimental/bits/timed_invoker.h \

include/experimental/bits/small_block_recycler.h
@@ -1,0 +1,134 @@
+//
+// small_block_recycler.h
+// ~~~~~~~~~~~~~~~~~~~~~~
+// Recycles small allocations that may be associated with a chain of operations.
+//
+// Copyright (c) 2014 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef EXECUTORS_EXPERIMENTAL_BITS_SMALL_BLOCK_RECYCLER_H
+#define EXECUTORS_EXPERIMENTAL_BITS_SMALL_BLOCK_RECYCLER_H
+
+#include <climits>
+#include <memory>
+#include <utility>
+
+namespace std {
+namespace experimental {
+
+template <unsigned char __max = UCHAR_MAX>
+class __small_block_recycler
+{
+public:
+  __small_block_recycler(const __small_block_recycler&) = delete;
+  __small_block_recycler& operator=(const __small_block_recycler&) = delete;
+
+  __small_block_recycler() : _M_memory(nullptr)
+  {
+  }
+
+  ~__small_block_recycler()
+  {
+    if (_M_memory)
+      ::operator delete(_M_memory);
+  }
+
+  template <class _T>
+  struct _Delete
+  {
+    constexpr _Delete() noexcept {}
+    template <class _U> _Delete(const _Delete<_U>&) {}
+    void operator()(_T* __p) const { _S_instance._Destroy(__p); }
+  };
+
+  template <class _T>
+  using _Unique_ptr = unique_ptr<_T, _Delete<_T>>;
+
+  template <class _T, class... _Args>
+  static _T* _Create(_Args&&... __args)
+  {
+    _Init __i;
+    __i._M_memory = __i._M_instance._Allocate(sizeof(_T));
+    __i._M_size = sizeof(_T);
+    _T* __p = new (__i._M_memory) _T(forward<_Args>(__args)...);
+    __i._M_memory = nullptr;
+    return __p;
+  }
+
+  template <class _T>
+  static void _Destroy(_T* __p)
+  {
+    __p->~_T();
+    _S_instance._Deallocate(__p, sizeof(_T));
+  }
+
+  void* _Allocate(size_t __size)
+  {
+    if (_M_memory)
+    {
+      void* const __p = _M_memory;
+      _M_memory = 0;
+
+      unsigned char* const __mem = static_cast<unsigned char*>(__p);
+      if (static_cast<size_t>(__mem[0]) >= __size)
+      {
+        __mem[__size] = __mem[0];
+        return __p;
+      }
+
+      ::operator delete(__p);
+    }
+
+    void* const __p = ::operator new(__size + 1);
+    unsigned char* const __mem = static_cast<unsigned char*>(__p);
+    __mem[__size] = (__size <= __max) ? static_cast<unsigned char>(__size) : 0;
+    return __p;
+  }
+
+  void _Deallocate(void* __p, size_t __size)
+  {
+    if (__p)
+    {
+      if (__size <= __max)
+      {
+        if (_M_memory == 0)
+        {
+          unsigned char* const __mem = static_cast<unsigned char*>(__p);
+          __mem[0] = __mem[__size];
+          _M_memory = __p;
+          return;
+        }
+      }
+
+      ::operator delete(__p);
+    }
+  }
+
+  static __small_block_recycler& _Instance()
+  {
+    return _S_instance;
+  }
+
+private:
+  struct _Init
+  {
+    __small_block_recycler& _M_instance = _S_instance;
+    void* _M_memory = nullptr;
+    size_t _M_size = 0;
+    ~_Init() { _M_instance._Deallocate(_M_memory, _M_size); }
+  };
+
+  void* _M_memory;
+  static thread_local __small_block_recycler _S_instance;
+};
+
+template <unsigned char __max>
+thread_local __small_block_recycler<__max> __small_block_recycler<__max>::_S_instance;
+
+} // namespace experimental
+} // namespace std
+
+#endif

include/experimental/bits/timer.h
@@ -245,7 +245,8 @@ auto basic_timer<_Clock, _TimerTraits>::async_wait(_CompletionToken&& __token)
   async_completion<_CompletionToken, void(error_code)> __completion(__token);
 
   typedef __wait_op<_Handler> _Op;
-  unique_ptr<_Op> __op(new _Op(std::move(__completion.handler)));
+  __small_block_recycler<>::_Unique_ptr<_Op> __op(
+    __small_block_recycler<>::_Create<_Op>(std::move(__completion.handler)));
 
   _M_service->_M_reactor._Enqueue_timer(_M_service->_M_queue, _M_expiry, _M_data, __op.get());
   __op.release();

commit 6121e6fa92a41bf1b5308a7cd2dff546bd7d366d

Fix bug in work counting introduced by db2bedc862d47da203c98c033f3003e02bf05d5b.

include/experimental/bits/scheduler.h
@@ -183,22 +183,33 @@ class __scheduler_op
   : public __operation
 {
 public:
+  __scheduler_op(const __scheduler_op&) = delete;
+  __scheduler_op& operator=(const __scheduler_op&) = delete;
+
   template <class _F> __scheduler_op(_F&& __f, __scheduler& __s)
-    : _M_func(forward<_F>(__f)), _M_owner(__s)
+    : _M_func(forward<_F>(__f)), _M_owner(&__s)
   {
+    _M_owner->_Work_started();
+  }
+
+  __scheduler_op(__scheduler_op&& __s)
+    : _M_func(std::move(__s._M_func)), _M_owner(__s._M_owner)
+  {
+    __s._M_owner = 0;
   }
 
   ~__scheduler_op()
   {
-    _M_owner._Work_finished();
+    if (_M_owner)
+      _M_owner->_Work_finished();
   }
 
   virtual void _Complete()
   {
     __small_block_recycler<>::_Unique_ptr<__scheduler_op> __op(this);
-    _Func __f(std::move(_M_func));
+    __scheduler_op __tmp(std::move(*this));
     __op.reset();
-    __f();
+    __tmp._M_func();
   }
 
   virtual void _Destroy()
@@ -208,7 +219,7 @@ public:
 
 private:
   _Func _M_func;
-  __scheduler& _M_owner;
+  __scheduler* _M_owner;
 };
 
 template <class _F> void __scheduler::_Post(_F&& __f)
@@ -220,7 +231,6 @@ template <class _F> void __scheduler::_Post(_F&& __f)
   lock_guard<mutex> lock(_M_mutex);
 
   _M_queue._Push(__op.get());
-  _Work_started();
 
   if (_M_queue._Front() == __op.get())
     _M_condition.notify_one();

include/experimental/bits/wait_op.h
@@ -42,7 +42,8 @@ public:
   virtual void _Complete()
   {
     __small_block_recycler<>::_Unique_ptr<__wait_op> __op(this);
-    auto __executor(get_executor(_M_work));
+    typename decltype(get_executor(declval<_Func>()))::work __work(std::move(_M_work));
+    auto __executor(get_executor(__work));
     __invoke_with_result<const error_code, _Func> __i{_M_ec, std::move(_M_func)};
     __op.reset();
     __executor.post(std::move(__i));