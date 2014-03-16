6f12eb69d2a511e09dbf3c2fa597258be924b640
Christopher Kohlhoff <chris@kohlhoff.com>
2014-03-16 12:47:12 +1100

    Add performance tests to measure context switching overhead.

---
+++ src/tests/performance/function_context_switch.cpp
@@ -0,0 +1,35 @@
+#include <experimental/loop_scheduler>
+#include <chrono>
+#include <iostream>
+
+using namespace std::experimental;
+
+const int chains = 4;
+const int iterations = 1000000;
+
+void chain(loop_scheduler::executor ex, int i)
+{
+  if (i < iterations)
+    post(ex.wrap([=]{ chain(ex, i + 1); }));
+}
+
+int main()
+{
+  loop_scheduler s;
+  auto ex = get_executor(s);
+
+  for (int c = 0; c < chains; ++c)
+    dispatch(ex.wrap([=]{ chain(ex, 0); }));
+
+  std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+  s.run();
+  std::chrono::steady_clock::duration elapsed = std::chrono::steady_clock::now() - start;
+
+  std::cout << "time per switch: ";
+  std::chrono::steady_clock::duration per_iteration = elapsed / iterations / chains;
+  std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(per_iteration).count();
+  std::cout << " nanoseconds\n";
+
+  std::cout << "switches per second: ";
+  std::cout << (std::chrono::seconds(1) / per_iteration) << "\n";
+}
---
+++ src/tests/performance/yield_context_switch.cpp
@@ -0,0 +1,40 @@
+#include <experimental/loop_scheduler>
+#include <experimental/yield>
+#include <chrono>
+#include <iostream>
+
+using namespace std::experimental;
+
+const int chains = 4;
+const int iterations = 1000000;
+
+int main()
+{
+  loop_scheduler s;
+  auto ex = get_executor(s);
+
+  for (int c = 0; c < chains; ++c)
+  {
+    dispatch(
+      ex.wrap(
+        [](basic_yield_context<loop_scheduler::executor> yield)
+        {
+          for (int i = 0; i < iterations; ++i)
+          {
+            post(yield);
+          }
+        }));
+  }
+
+  std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+  s.run();
+  std::chrono::steady_clock::duration elapsed = std::chrono::steady_clock::now() - start;
+
+  std::cout << "time per switch: ";
+  std::chrono::steady_clock::duration per_iteration = elapsed / iterations / chains;
+  std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(per_iteration).count();
+  std::cout << " nanoseconds\n";
+
+  std::cout << "switches per second: ";
+  std::cout << (std::chrono::seconds(1) / per_iteration) << "\n";
+}
