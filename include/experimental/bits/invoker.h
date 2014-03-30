d7beae1f4cd62be8ab11b838d10ec9b7125e46ca
Christopher Kohlhoff <chris@kohlhoff.com>
2014-03-30 17:13:23 +1100

    Remove unused code.

--- include/experimental/bits/invoker.h
+++ include/experimental/bits/invoker.h
@@ -17,53 +17,6 @@
 namespace std {
 namespace experimental {
 
-template <class _Handler>
-class __invoker
-{
-public:
-  template <class _H> explicit __invoker(_H&& __h)
-    : _M_handler(forward<_H>(__h)),
-      _M_handler_work(make_executor(_M_handler).make_work())
-  {
-  }
-
-  template <class... _Args> void operator()(_Args&&... __args)
-  {
-    make_executor(_M_handler_work).dispatch(
-      _Make_tuple_invoker(std::move(_M_handler), forward<_Args>(__args)...));
-  }
-
-private:
-  _Handler _M_handler;
-  typename decltype(make_executor(declval<_Handler>()))::work _M_handler_work;
-};
-
-struct __invoker_func_executor
-{
-  template <class _Func, class _Handler>
-  static auto _Get(_Func& __f, _Handler&)
-  {
-    return make_executor(__f);
-  }
-};
-
-struct __invoker_handler_executor
-{
-  template <class _Func, class _Handler>
-  static auto _Get(_Func&, _Handler& __h)
-  {
-    return make_executor(__h);
-  }
-};
-
-template <class _Func, class _Handler>
-inline auto __make_invoker_executor(_Func& __f, _Handler& __h)
-{
-  typedef decltype(make_executor(__f)) _FuncExecutor;
-  return conditional<is_same<_FuncExecutor, system_executor>::value,
-    __invoker_handler_executor, __invoker_func_executor>::type::_Get(__f, __h);
-}
-
 template <class _Signature, class... _CompletionTokens>
 class __invoker_head;
 
