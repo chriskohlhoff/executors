54e09df1fbdd67904db4467be773e55b48601d3d
Christopher Kohlhoff <chris@kohlhoff.com>
2014-05-27 00:49:43 +1000

    Add defer() function.
    Initial implementation is a clone of post(), and does not yet take
    advantage of the available optimisation when scheduling a continuation
    of the current caller.

---
+++ include/experimental/bits/defer.h
@@ -0,0 +1,54 @@
+//
+// defer.h
+// ~~~~~~~
+// Schedule a function to run later, treating it as a continuation of the caller.
+//
+// Copyright (c) 2014 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef EXECUTORS_EXPERIMENTAL_BITS_DEFER_H
+#define EXECUTORS_EXPERIMENTAL_BITS_DEFER_H
+
+#include <experimental/bits/invoker.h>
+
+namespace std {
+namespace experimental {
+
+template <class... _CompletionTokens>
+typename __invoke_without_executor<_CompletionTokens...>::_Result
+  defer(_CompletionTokens&&... __tokens)
+{
+  static_assert(sizeof...(_CompletionTokens) > 0,
+    "defer() must be called with one or more completion tokens");
+
+  __invoker_head<void(), _CompletionTokens...> __head(__tokens...);
+  async_result<__invoker_head<void(), _CompletionTokens...>> __result(__head);
+
+  auto __completion_executor(__head._Make_initial_executor());
+  __completion_executor.defer(std::move(__head));
+
+  return __result.get();
+}
+
+template <class _Executor, class... _CompletionTokens>
+typename __invoke_with_executor<_Executor, _CompletionTokens...>::_Result
+  defer(_Executor&& __e, _CompletionTokens&&... __tokens)
+{
+  static_assert(sizeof...(_CompletionTokens) > 0,
+    "defer() must be called with one or more completion tokens");
+
+  __invoker_head<void(), _CompletionTokens...> __head(__tokens...);
+  async_result<__invoker_head<void(), _CompletionTokens...>> __result(__head);
+
+  __e.defer(std::move(__head));
+
+  return __result.get();
+}
+
+} // namespace experimental
+} // namespace std
+
+#endif
