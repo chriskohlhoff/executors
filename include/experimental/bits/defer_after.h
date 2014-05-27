fb68a741d561f80e702e8d8d44fb32db11c4148f
Christopher Kohlhoff <chris@kohlhoff.com>
2014-05-27 16:26:18 +1000

    Add defer_at() and defer_after().

---
+++ include/experimental/bits/defer_after.h
@@ -0,0 +1,57 @@
+//
+// defer_after.h
+// ~~~~~~~~~~~~~
+// Schedule a function to run at a relative time.
+//
+// Copyright (c) 2014 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef EXECUTORS_EXPERIMENTAL_BITS_DEFER_AFTER_H
+#define EXECUTORS_EXPERIMENTAL_BITS_DEFER_AFTER_H
+
+#include <experimental/bits/invoker.h>
+#include <experimental/bits/timed_invoker.h>
+
+namespace std {
+namespace experimental {
+
+template <class _Rep, class _Period, class... _CompletionTokens>
+typename __invoke_without_executor<_CompletionTokens...>::_Result
+  defer_after(const chrono::duration<_Rep, _Period>& __rel_time,
+    _CompletionTokens&&... __tokens)
+{
+  static_assert(sizeof...(_CompletionTokens) > 0,
+    "defer_after() must be called with one or more completion tokens");
+
+  __timed_invoker<chrono::steady_clock, _CompletionTokens...> __head(__tokens...);
+  async_result<__invoker_tail<void(), _CompletionTokens...>> __result(__head._Get_tail());
+
+  auto __completion_executor(__head._Get_tail().get_executor());
+  __head._Start(__completion_executor, __rel_time);
+
+  return __result.get();
+}
+
+template <class _Rep, class _Period, class _Executor, class... _CompletionTokens>
+typename __invoke_with_executor<_Executor, _CompletionTokens...>::_Result
+  defer_after(const chrono::duration<_Rep, _Period>& __rel_time,
+    _Executor&& __e, _CompletionTokens&&... __tokens)
+{
+  static_assert(sizeof...(_CompletionTokens) > 0,
+    "defer_after() must be called with one or more completion tokens");
+
+  __timed_invoker<chrono::steady_clock, _CompletionTokens...> __head(__tokens...);
+  async_result<__invoker_tail<void(), _CompletionTokens...>> __result(__head._Get_tail());
+
+  __head._Start(__e, __rel_time);
+
+  return __result.get();
+}
+
+} // namespace experimental
+} // namespace std
+
+#endif
---
+++ include/experimental/bits/defer_at.h
@@ -0,0 +1,57 @@
+//
+// defer_at.h
+// ~~~~~~~~~~
+// Schedule a function to run at an absolute time.
+//
+// Copyright (c) 2014 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef EXECUTORS_EXPERIMENTAL_BITS_DEFER_AT_H
+#define EXECUTORS_EXPERIMENTAL_BITS_DEFER_AT_H
+
+#include <experimental/bits/invoker.h>
+#include <experimental/bits/timed_invoker.h>
+
+namespace std {
+namespace experimental {
+
+template <class _Clock, class _Duration, class... _CompletionTokens>
+typename __invoke_without_executor<_CompletionTokens...>::_Result
+  defer_at(const chrono::time_point<_Clock, _Duration>& __abs_time,
+    _CompletionTokens&&... __tokens)
+{
+  static_assert(sizeof...(_CompletionTokens) > 0,
+    "defer_at() must be called with one or more completion tokens");
+
+  __timed_invoker<_Clock, _CompletionTokens...> __head(__tokens...);
+  async_result<__invoker_tail<void(), _CompletionTokens...>> __result(__head._Get_tail());
+
+  auto __completion_executor(__head._Get_tail().get_executor());
+  __head._Start(__completion_executor, __abs_time);
+
+  return __result.get();
+}
+
+template <class _Clock, class _Duration, class _Executor, class... _CompletionTokens>
+typename __invoke_with_executor<_Executor, _CompletionTokens...>::_Result
+  defer_at(const chrono::time_point<_Clock, _Duration>& __abs_time,
+    _Executor&& __e, _CompletionTokens&&... __tokens)
+{
+  static_assert(sizeof...(_CompletionTokens) > 0,
+    "defer_at() must be called with one or more completion tokens");
+
+  __timed_invoker<_Clock, _CompletionTokens...> __head(__tokens...);
+  async_result<__invoker_tail<void(), _CompletionTokens...>> __result(__head._Get_tail());
+
+  __head._Start(__e, __abs_time);
+
+  return __result.get();
+}
+
+} // namespace experimental
+} // namespace std
+
+#endif
